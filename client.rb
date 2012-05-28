b4d441a82354a8b0c78cbad6ca98847b4dd87700
Gregor Fabritius <gre@g0r.de>
2012-05-28 17:54:32 +0200

    basic communication established.

--- client.rb
+++ client.rb
@@ -1,10 +1,29 @@
+require 'rubygems'
 require 'socket'
+require 'sinatra'
+require 'json'
+require 'pp'
 
-def broadcast_msg(message, port)
+
+def broadcast_msg(message, udp_port)
   s = UDPSocket.new
   s.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
-  s.send(message, 0, '<broadcast>', port)
+  s.send(message, 0, '<broadcast>', udp_port)
 end
 
-broadcast_msg "mass-ping", 1234
+
+post '/queen' do
+  data = JSON.parse request.body.read
+
+  puts "Queen told me:"
+  PP.pp data
+
+  {:status => "received"}.to_json
+end
+
+sleep 1
+udp_port = 1234
+http_port = 4567
+message = {:type => "drone", :port => http_port}
+broadcast_msg Marshal.dump(message), udp_port
 
--- server.rb
+++ server.rb
@@ -1,12 +1,41 @@
+require 'rubygems'
+require 'json'
 require 'socket'
 require 'pp'
+require 'rest-client'
 
-s = UDPSocket.new
-s.bind('0.0.0.0', 1234)
 
-loop do
-  text, sender = s.recvfrom(16)
-  puts text
-  PP.pp sender
+udp_port = 1234
+
+
+def inform_drone(ip, port)
+  url = "http://#{ip}:#{port}/queen"
+  PP.pp url
+  RestClient.post url, {:message => "hello"}.to_json
 end
 
+Thread.fork do
+  s = UDPSocket.new
+  s.bind('0.0.0.0', udp_port)
+
+  loop do
+    text, sender = s.recvfrom(1024)
+    data = Marshal.load(text)
+    PP.pp data
+    PP.pp sender
+
+    ip = sender[3]
+    PP.pp ip
+
+    port = data[:port]
+    PP.pp port
+
+    sleep 3
+    begin
+      inform_drone ip, port
+    rescue
+    end
+  end
+end
+
+puts gets
